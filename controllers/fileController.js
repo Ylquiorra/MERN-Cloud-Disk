4b1d71f1e668185c4bb986c24703a060483865df
Vladislav <vladislav0sidorov.gmail.com>
2022-11-29 19:41:36 +0300

    Added the ability to add files to the server

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,9 @@
+const config = require("config");
 const fileService = require('../services/fileService')
+const fs = require('fs')
 const User = require('../models/User');
 const File = require('../models/File');
+
 
 class FileController {
   async createDir(req, res) {
@@ -34,6 +37,51 @@ class FileController {
     } catch (error) {
       console.log(error)
       return res.status(500).json({ error: 'Error getting files' })
+    }
+  }
+
+  async uploadFile(req, res) {
+    try {
+      const file = req.files.file
+
+      const parent = await File.findOne({ user: req.user.id, _id: req.body.parent })
+      const user = await User.findOne({ _id: req.user.id })
+
+      if (user.usedSpace + file.size > user.diskSpace) {
+        return res.staus(400).json({ message: 'Space exceeded' })
+      }
+
+      user.usedSpace = user.usedSpace + file.size
+
+      let path;
+      if (parent) {
+        path = `${config.get('filePath')}\\${user._id}\\${parent.path}\\${file.name}`
+      } else {
+        path = `${config.get('filePath')}\\${user._id}\\${file.name}`
+      }
+
+      if (fs.existsSync(path)) {
+        return res.status(400).json({ message: 'File already exists' })
+      }
+      file.mv(path)
+
+      const type = file.name.split('.').pop()
+      const dbFile = new File({
+        name: file.name,
+        type,
+        size: file.size,
+        path: parent?.path,
+        parent: parent?._id,
+        user: user._id
+      })
+
+      await dbFile.save()
+      await user.save()
+
+      res.json(dbFile)
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ error: 'Error uploading file' })
     }
   }
 }
